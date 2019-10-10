291a4815f65ad8aeb5e3965952df64dd1d4332b0
jenna729 <56374739+jenna729@users.noreply.github.com>
2019-10-10 14:43:32 +0800

    Add files via upload

---
+++ precache-manifest.bc8e2c196856622af7931455e76f7edd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e07a6903f80681569d2c99fddd47633",
+    "url": "/static/media/skill-pic.8e07a690.jpg"
+  },
+  {
+    "revision": "a4aec994dd09476d01c198edcf3e570d",
+    "url": "/static/media/me2.a4aec994.jpg"
+  },
+  {
+    "revision": "d117ba61afbe9fab1786a171897cf8f8",
+    "url": "/static/media/me.d117ba61.jpg"
+  },
+  {
+    "revision": "7b26e6fd9d9f784d722aa2d9c99ce520",
+    "url": "/static/media/experience-pic.7b26e6fd.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "824b60e2ff31a23643fb",
+    "url": "/static/js/main.98384c5a.chunk.js"
+  },
+  {
+    "revision": "75f3f92aceb066e1953c",
+    "url": "/static/js/2.aaa51163.chunk.js"
+  },
+  {
+    "revision": "824b60e2ff31a23643fb",
+    "url": "/static/css/main.63b67d93.chunk.css"
+  },
+  {
+    "revision": "cf18749cad44e0b6c1a31d25e7d0a1b4",
+    "url": "/index.html"
+  }
+];
